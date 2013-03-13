66d4fd0ae6d2f94274d55e2f0b0b1e890608d45a
Dimas Aryo <mr.dimasaryo@gmail.com>
2013-03-13 14:22:09 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlackSwan::Application.config.secret_token = 'f5b836a409974bc807841203b55869b7cd053ca88866d254c66bc8314b898b2fa1e748e4ddef4215d61f8df8b42ed126b4971dc050350fa90507830b76de9c1b'
